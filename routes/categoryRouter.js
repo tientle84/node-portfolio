cd9558a0a8e49717914f6fe3ab9cdf2045de5aaf
tientle84 <alctdl00@gmail.com>
2021-08-27 13:13:45 -0400

    fix routers

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -2,53 +2,53 @@ const express = require("express");
 const categoryRouter = express.Router();
 
 categoryRouter
-  .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end("Will send all the categories to you");
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the category: ${req.body.name} with description: ${req.body.description}`
-    );
-  })
-  .put((req, res) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /categories");
-  })
-  .delete((req, res) => {
-    res.end("Deleting all categories");
-  });
+    .route("/")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        next();
+    })
+    .get((req, res) => {
+        res.end("Will send all the categories to you");
+    })
+    .post((req, res) => {
+        res.end(
+            `Will add the category: ${req.body.name} with description: ${req.body.description}`
+        );
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end("PUT operation not supported on /categories");
+    })
+    .delete((req, res) => {
+        res.end("Deleting all categories");
+    });
 
 categoryRouter
-  .route("/:producId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end(
-      `Will send details of the category: ${req.params.categoryId} to you`
-    );
-  })
-  .post((req, res) => {
-    res.statusCode = 403;
-    res.end(
-      `POST operation not supported on /categories/${req.params.categoryId}`
-    );
-  })
-  .put((req, res) => {
-    res.write(`Updating the category: ${req.params.categoryId}\n`);
-    res.end(`Will update the category: ${req.body.name}
+    .route("/:categoryId")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        next();
+    })
+    .get((req, res) => {
+        res.end(
+            `Will send details of the category: ${req.params.categoryId} to you`
+        );
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(
+            `POST operation not supported on /categories/${req.params.categoryId}`
+        );
+    })
+    .put((req, res) => {
+        res.write(`Updating the category: ${req.params.categoryId}\n`);
+        res.end(`Will update the category: ${req.body.name}
         with description: ${req.body.description}`);
-  })
-  .delete((req, res) => {
-    res.end(`Deleting category: ${req.params.categoryId}`);
-  });
+    })
+    .delete((req, res) => {
+        res.end(`Deleting category: ${req.params.categoryId}`);
+    });
 
 module.exports = categoryRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,49 +2,49 @@ const express = require("express");
 const userRouter = express.Router();
 
 userRouter
-  .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end("Will send all the users to you");
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the user: ${req.body.name} with email: ${req.body.email}`
-    );
-  })
-  .put((req, res) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /users");
-  })
-  .delete((req, res) => {
-    res.end("Deleting all users");
-  });
+    .route("/")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        next();
+    })
+    .get((req, res) => {
+        res.end("Will send all the users to you");
+    })
+    .post((req, res) => {
+        res.end(
+            `Will add the user: ${req.body.name} with email: ${req.body.email}`
+        );
+    })
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end("PUT operation not supported on /users");
+    })
+    .delete((req, res) => {
+        res.end("Deleting all users");
+    });
 
 userRouter
-  .route("/:producId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end(`Will send details of the user: ${req.params.userId} to you`);
-  })
-  .post((req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /users/${req.params.userId}`);
-  })
-  .put((req, res) => {
-    res.write(`Updating the user: ${req.params.userId}\n`);
-    res.end(`Will update the user: ${req.body.name}
+    .route("/:userId")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        next();
+    })
+    .get((req, res) => {
+        res.end(`Will send details of the user: ${req.params.userId} to you`);
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /users/${req.params.userId}`);
+    })
+    .put((req, res) => {
+        res.write(`Updating the user: ${req.params.userId}\n`);
+        res.end(`Will update the user: ${req.body.name}
         with email: ${req.body.email}`);
-  })
-  .delete((req, res) => {
-    res.end(`Deleting user: ${req.params.userId}`);
-  });
+    })
+    .delete((req, res) => {
+        res.end(`Deleting user: ${req.params.userId}`);
+    });
 
 module.exports = userRouter;
